47d4d508ab3bebb0474d32b2d901cf3e6d188313
Abdul Rehman Aziz <42958150+abdulrehman1363@users.noreply.github.com>
2024-06-30 17:10:50 +0500

    blog posts using AI updated env.js

--- server/services/api.js
+++ server/services/api.js
@@ -2,16 +2,12 @@ const axios = require("axios");
 
 const fetchFromAPI = async (body, api_key, url) => {
   try {
-    //console.log('body', JSON.stringify(body));
-    //console.log('api_key', api_key);
     const response = await axios.post(url, body, {
       headers: {
         "Content-Type": "application/json",
         "Authorization": "Bearer "+ api_key 
       },
     });
-    //console.log('response', response);
-    //console.log('response', response.data);
     return response.data;
   } catch (error) {
     console.log(error);
